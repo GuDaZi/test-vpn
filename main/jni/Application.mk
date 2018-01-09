abb484a69dcf731935f8f02af7ed2788902ab717
Heda <512505803@qq.com>
2018-01-09 09:19:10 +0800

    first commit

---
+++ main/jni/Application.mk
@@ -0,0 +1,21 @@
+ifeq ($(USE_SHORT_COMMANDS),1)
+    APP_SHORT_COMMANDS := true
+endif
+
+APP_ABI := arm64-v8a armeabi armeabi-v7a x86 x86_64
+APP_PLATFORM := android-14
+#NDK_TOOLCHAIN_VERSION := 4.9
+
+#APP_STL:=stlport_static
+APP_STL:=c++_static
+
+
+#APP_OPTIM := release
+
+NDK_TOOLCHAIN_VERSION=4.9
+APP_CPPFLAGS += -std=c++1y
+APP_CFLAGS += -std=c99
+#APP_CPPFLAGS += -std=gnu99
+APP_CFLAGS += -funwind-tables
+
+
